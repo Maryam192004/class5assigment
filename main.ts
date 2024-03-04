cf19d3fea1c2256d5ca5d3b31912d14c39852c55
Maryam192004 <159527981+Maryam192004@users.noreply.github.com>
2024-03-04 22:35:54 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,15 @@
+//example 1 string
+var message = "Maryam";
+console.log(message);
+//exammple 2 number
+var age = 18;
+console.log(age);
+//example 3 boolean
+var rain = true;
+console.log(rain);
+// example 4 null
+var x = null;
+console.log(x);
+// example 5 undefined
+var a;
+console.log(a);
